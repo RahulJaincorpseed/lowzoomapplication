25b4b54cd5616f327645b0f6b1fbbcfa450e6103
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-12 12:56:53 +0530

    add user signup functionality

---
+++ src/Api/UserApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const getOtp = (mobile) =>{
+    console.log(mobile.current.value);
+    let value = {"mobile": mobile}
+    const getOtpApi = async () =>{
+      try{
+      const data = await axios.post("http://localhost:8080/api/auth/otp", value);
+      console.log(data);
+      }
+      catch(err){
+        console.log(err);
+      }
+    }
+    getOtpApi();
+  }
--- src/Login/SignUp.js
+++ src/Login/SignUp.js
@@ -1,11 +1,86 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 import "./login.scss"
 import Glogo from "..//images/google-logo.png"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const SignUp = () => {
-  const [checkCircle, setCheckCircle] = useState(false)
-  console.log(checkCircle)
+  const [checkCircle, setCheckCircle] = useState(false);
+  const [error, setError] = useState(false);
+  const [onetp, setOnetp] = useState(""); 
+  const navigate = useNavigate();
+  
+
+  const mobileNumber = useRef();
+  const OTP = useRef();
+  const passowrd = useRef();
+
+  const getOtp = () =>{
+    console.log(mobileNumber.current.value);
+    if(mobileNumber.current.value === ""){
+      mobileNumber.current.style.border = "1px solid red";
+      setError(true);
+      return
+    }
+    else{
+    let value = {"mobile": mobileNumber.current.value}
+    const getOtpApi = async () =>{
+      try{
+      const data = await axios.post("http://localhost:8080/api/auth/otp", value);
+      console.log(data.data);
+      setOnetp(data.data)
+      }
+      catch(err){
+        setError(err.message)
+      }
+    }
+    getOtpApi();
+  }
+  }
+
+  console.log("i am state", onetp, onetp.mobile, onetp.otp);
+
+
+  const userSignUp = () =>{
+    if(mobileNumber.current.value === ""){
+      mobileNumber.current.style.border = "1px solid red";
+      setError(true);
+      return
+    }
+    else if(OTP.current.value === ""){
+      OTP.current.style.border = "1px solid red";
+      setError(true);
+      return
+    }
+    else if(passowrd.current.value === ""){
+      passowrd.current.style.border = "1px solid red";
+      setError(true);
+      return
+    }
+    else{
+    const signUpByuser = async () =>{
+      try{
+      let value = {"mobile": onetp.mobile, "otp": onetp.otp, password: passowrd.current.value};
+      const userData =  await axios.post("http://localhost:8080/api/auth/signup", value)
+      if(userData.data.statusCode === 200){
+        console.log("i am user data", userData);
+      }
+      else{
+        console.log("user already", userData)
+      }
+      }
+      catch(err){
+        console.log(err);
+      }
+    }
+    signUpByuser()
+  }
+
+  }
+
+
+
+
   return (
     <div className="sign-up container">
       <div className="sign-box">
@@ -20,13 +95,34 @@ const SignUp = () => {
         <p className="signin-text">Or Sign In With Email</p>
         <div>
           <label className="label-heading" htmlFor="phone">
-            User ID *
+            Mobile Number*
           </label>
           <input
             type="tel"
             id="phone"
+            ref={mobileNumber}
             name="phone"
             placeholder="+91 9999008078"
+            required
+          />
+          {error ? <span>Mobile Number can not be blank</span>: ""}
+           <button
+              onClick={getOtp}
+              className="btn btn-outline-primary sign-button w-100 my-3"
+            >
+              GET OTP
+            </button>
+        </div>
+        <div>
+          <label className="label-heading" htmlFor="otp">
+            OTP *
+          </label>
+          <input
+            type="text"
+            ref={OTP}
+            id="otp"
+            name="otp"
+            placeholder="Enter your OTP"
             required
           />
         </div>
@@ -36,11 +132,12 @@ const SignUp = () => {
           </label>
           <input
             className="password-input"
+            ref={passowrd}
             type="password"
             id="password"
             name="password"
             placeholder="Min. 8 Charecter"
-            autocomplete="off"
+            autoComplete="off"
             required
           />
         </div>
@@ -55,12 +152,13 @@ const SignUp = () => {
             <p className="label-heading">Remember me</p>
           </div>
           <div>
-            <Link
+            <button
               to={`/userinfo`}
+              onClick={userSignUp}
               className="btn btn-outline-primary sign-button"
             >
               Sign Up
-            </Link>
+            </button>
           </div>
         </div>
       </div>
