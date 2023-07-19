d84cd3dc56b52ec4e8d2fcc19026b0a820c6e9f7
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-19 11:03:58 +0530

    add model in center

--- src/ManageLicence/ManageComplience/TaskManagement.js
+++ src/ManageLicence/ManageComplience/TaskManagement.js
@@ -1,46 +1,125 @@
 import React from "react"
+import "./TaskManagement.css"
 
 const TaskManagement = () => {
   return (
-    <div
-      class="modal fade"
-      id="TaskManagementModel"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">
-              Modal title
-            </h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">...</div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">
-              Save changes
-            </button>
+    <div className="task-model-box">
+        <div
+          className="modal fade"
+          id="TaskManagementModel"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalCenterTitle"
+          aria-hidden="true"
+        >
+          <div
+            className="modal-dialog mod-center modal-dialog-centered"
+            role="document"
+          >
+            <div className="modal-content all-center overflow-task">
+              <div className="add-team-body body-task">
+                {/* START */}
+                <div className="personal-info container">
+                  <h4 className="info-text">Task Management</h4>
+                  <div className="first-form form-row">
+                    <div className="form-group col-md-6">
+                      <div className="pr-ten">
+                        <label className="label-heading" htmlFor="fullName">
+                          Full Name *
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control input-focus"
+                          id="fullName"
+                          placeholder="Full Name"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <div className="pl-ten">
+                        <label className="label-heading" htmlFor="accessTypes">
+                          Access Types*
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control input-focus"
+                          id="accessTypes"
+                          placeholder="Access Types"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <div className="pr-ten">
+                        <label className="label-heading" htmlFor="email">
+                          Email ID *
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control input-focus"
+                          id="email"
+                          placeholder="Email ID"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <div className="pl-ten">
+                        <label className="label-heading" htmlFor="mobileNumber">
+                          Mobile Number*
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control input-focus"
+                          id="mobileNumber"
+                          placeholder="Mobile Number"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <div className="pr-ten">
+                        <label className="label-heading" htmlFor="resources">
+                          Types of Resources*
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control input-focus"
+                          id="resources"
+                          placeholder="Types of Resources"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <div className="pl-ten">
+                        <label className="label-heading" htmlFor="addpeople">
+                          Add People*
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control input-focus"
+                          id="addpeople"
+                          placeholder="Add People"
+                        />
+                      </div>
+                    </div>
+                    <div className="all-between-items">
+                      <div className="all-center">
+                        <i className="fa-solid fa-gear"></i>
+                        <h2>Advanced Setting</h2>
+                      </div>
+                      <div>
+                        <button className="first-button form-prev-btn">
+                          Submit
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+
   )
 }
 
