aef2162242ba0ecd180cb3341896ee9e3dab2a52
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-24 11:54:58 +0530

    order-ui-completed

--- src/ManageLicence/VendorTask/Order/Order.js
+++ src/ManageLicence/VendorTask/Order/Order.js
@@ -2,7 +2,52 @@ import React from "react"
 import "./Order.scss"
 
 const Order = () => {
-  return <div>Order</div>
+  const data =  [
+    {serviceName: "CONSENT TO ESTABLISHMENT", action:"DOCUMENTS", orderNo: "221028704798", email:"abc@bharatpe.com", status: "IN PROGRESS"},
+    {serviceName: "CONSENT TO ESTABLISHMENT", action:"DOCUMENTS", orderNo: "221028704798", email:"abc@bharatpe.com", status: "IN PROGRESS"},
+    {serviceName: "CONSENT TO ESTABLISHMENT", action:"DOCUMENTS", orderNo: "221028704798", email:"abc@bharatpe.com", status: "IN PROGRESS"},
+    {serviceName: "CONSENT TO ESTABLISHMENT", action:"DOCUMENTS", orderNo: "221028704798", email:"abc@bharatpe.com", status: "IN PROGRESS"},
+    {serviceName: "CONSENT TO ESTABLISHMENT", action:"DOCUMENTS", orderNo: "221028704798", email:"abc@bharatpe.com", status: "IN PROGRESS"},
+    {serviceName: "CONSENT TO ESTABLISHMENT", action:"DOCUMENTS", orderNo: "221028704798", email:"abc@bharatpe.com", status: "IN PROGRESS"},
+    {serviceName: "CONSENT TO ESTABLISHMENT", action:"DOCUMENTS", orderNo: "221028704798", email:"abc@bharatpe.com", status: "IN PROGRESS"},
+  ]
+  return (
+    <div className="your-order-list">
+      <h2 className="vendor-heading">
+        <i class="fa-solid mr-2 fa-circle-exclamation"></i> Vendor Outsourced
+        Task
+      </h2>
+
+      <div className="vender-order-table table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">SERVICE NAME</th>
+              <th scope="col">ACTION</th>
+              <th scope="col">Order No.</th>
+              <th scope="col">Email Address</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((d, index)=>(
+              <tr key={index}>
+              <td>{d.serviceName}</td>
+              <td>{d.action}</td>
+              <td>{d.orderNo}</td>
+              <td>{d.email}</td>
+              <td>{d.status}</td>
+              <td><button className="vender-doc-btn">Documents</button></td>
+              <td><butoon className="vender-chat-btn">Chat</butoon></td>
+        
+            </tr>
+            ))}
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export default Order
