258925496be4e2ab2fbcea92083799156b343d29
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-08-16 17:04:11 +0530

    add navbar ui design

--- src/common/HomeNavbar.js
+++ src/common/HomeNavbar.js
@@ -1,11 +1,43 @@
 import React from "react";
 import logoImage from "../images/lowZoom.png";
+import "./Homenavbar.scss"
+import { Link } from "react-router-dom";
 
 const HomeNavbar = () => {
   return (
     <div className="home-nav">
         <div className="main-logo">
+            <img src={logoImage} />
+        </div>
+        <div className="home-links">
+            <div className="link-box">
+            <Link to="/services" className="link-itemsss">Services<i class="fa-solid ml-1 fa-chevron-down"></i></Link>
+            <div className="link-item">
+                <h1>item 1</h1>
+                <p>lorem2kns sdklfmsd lksdcns 
+                </p>
+            </div>
+            </div>
+            <div className="link-box">
+            <Link to="/services" className="link-itemsss">Partner<i class="fa-solid ml-1 fa-chevron-down"></i></Link>
+            <div className="link-item">
+                <h1>item 2</h1>
+                <p>lorem2kns sdklfmsd lksdcns 
+                </p>
+            </div>
+            </div>
+            <div className="link-box">
+            <Link to="/services" className="link-itemsss">Company<i class="fa-solid ml-1 fa-chevron-down"></i></Link>
+            <div className="link-item">
+                <h1>item 3</h1>
+                <p>lorem2kns sdklfmsd lksdcns 
+                </p>
+            </div>
+            </div>
+            <div className="link-box">
+            <Link to="/login" className="link-itemsss fw-600">Login</Link>
             
+            </div>
         </div>
     </div>
   )
