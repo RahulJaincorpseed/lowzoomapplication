50709da0adb378a3269879f3e5d0df611b3d1065
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-08-21 11:03:27 +0530

    add navtabs

--- src/common/HomeNavbar.js
+++ src/common/HomeNavbar.js
@@ -1,48 +1,108 @@
-import React from "react";
-import logoImage from "../images/lowZoom.png";
+import React from "react"
+import logoImage from "../images/lowZoom.png"
 import "./Homenavbar.scss"
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import NavTabItem from "../components/NavTabItem"
 
 const HomeNavbar = () => {
   return (
     <div className="container">
-    <div className="home-nav">
+      <div className="home-nav">
         <div className="main-logo">
-            <img src={logoImage} />
+          <img src={logoImage} />
         </div>
         <div className="home-links">
-            <div className="link-box">
-            <Link to="/services" className="link-itemsss">Services<i className="fa-solid ml-1 fa-chevron-down"></i></Link>
+          <div className="link-box">
+            <Link to="/services" className="link-itemsss">
+              Services<i className="fa-solid ml-1 fa-chevron-down"></i>
+            </Link>
             <div className="link-item">
-                <h1>item 1</h1>
-                <p>lorem2kns sdklfmsd lksdcns 
-                </p>
+              <div className="serviecs">
+                <NavTabItem
+                  icon={<i class="fa-solid fa-earth-americas"></i>}
+                  heading="Import Financing"
+                  description="Engages in importing goods from international markets, we can extend credit limits without requiring collateral, allowing you up to 120 days of credit."
+                />
+                <NavTabItem
+                  icon={<i class="fa-solid fa-map-location"></i>}
+                  heading="Export Financing"
+                  description="Export Financing For credit-based sales to buyers, we cover the credit period and safeguard you against any potential payment defaults by the buyer."
+                />
+                <NavTabItem
+                  icon={<i class="fa-solid fa-paperclip"></i>}
+                  heading="Domestic Trade Financing"
+                  description="Domestic Trade Financing Engaging in domestic credit-based sales? Our financing solutions ensure your credit period is covered, mitigating risks of buyer payment defaults "
+                />
+                <NavTabItem
+                  icon={<i class="fa-solid fa-hand-holding-dollar"></i>}
+                  heading="Working Capital Financing"
+                  description="Working Capital Financing immediate funds to manage your inventory, cover payroll, make supplier payments, and handle other operational expenses. "
+                />
+              </div>
             </div>
-            </div>
-            <div className="link-box">
-            <Link to="/services" className="link-itemsss">Partner<i className="fa-solid ml-1 fa-chevron-down"></i></Link>
+          </div>
+          <div className="link-box">
+            <Link to="/services" className="link-itemsss">
+              Partner<i className="fa-solid ml-1 fa-chevron-down"></i>
+            </Link>
             <div className="link-item">
-                <h1>item 2</h1>
-                <p>lorem2kns sdklfmsd lksdcns 
-                </p>
+              <div className="serviecs">
+                <NavTabItem
+                  icon={<i class="fa-solid fa-earth-americas"></i>}
+                  heading="Become Investor"
+                  description="Broaden your investment portfolio with trade finance opportunities at an institutional level"
+                />
+                <NavTabItem
+                  icon={<i class="fa-solid fa-map-location"></i>}
+                  heading="Become Channel Partner"
+                  description="Become Channel Partner Capitalize your connections with exporters by enrolling in our Channel Partnership Program "
+                />
+              </div>
             </div>
-            </div>
-            <div className="link-box">
-            <Link to="/services" className="link-itemsss">Company<i className="fa-solid ml-1 fa-chevron-down"></i></Link>
+          </div>
+          <div className="link-box">
+            <Link to="/services" className="link-itemsss">
+              Company<i className="fa-solid ml-1 fa-chevron-down"></i>
+            </Link>
             <div className="link-item">
-                <h1>item 3</h1>
-                <p>lorem2kns sdklfmsd lksdcns 
-                </p>
+              <div className="serviecs">
+                <NavTabItem
+                  icon={<i class="fa-solid fa-earth-americas"></i>}
+                  heading="About Company"
+                  description="At Tradbee®, we are committed to offer affordable and convenient trade finance solutions."
+                />
+                <NavTabItem
+                  icon={<i class="fa-solid fa-map-location"></i>}
+                  heading="Contact"
+                  description="Still seeking clarity on our process? engage with our Export Finance Advisor "
+                />
+                <NavTabItem
+                  icon={<i class="fa-solid fa-paperclip"></i>}
+                  heading="Careers"
+                  description="Work with Tradbee, we invest in the power of people to move the world forward.1"
+                />
+                <NavTabItem
+                  icon={<i class="fa-solid fa-hand-holding-dollar"></i>}
+                  heading="Knowledge Center"
+                  description="Elevate your product understanding with comprehensive feature overviews and insights."
+                />
+              </div>
+              <div className="policies-box">
+                <h4 className="policy-text">Privacy Policy</h4>
+                <h4 className="policy-text">Terms of use and cookie policy</h4>
+                <h4 className="policy-text">Cookie Setting</h4>
+              </div>
             </div>
-            </div>
-            <div className="link-box">
-            <Link to="/login" className="link-itemsss fw-600">Login</Link>
-            
-            </div>
+          </div>
+          <div className="link-box">
+            <Link to="/login" className="link-itemsss fw-600">
+              Login
+            </Link>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
   )
-};
+}
 
-export default HomeNavbar;
+export default HomeNavbar
---
+++ src/components/NavTabItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavTabItem.scss";
+import { Link } from "react-router-dom";
+
+const NavTabItem = ({heading, description, icon}) => {
+  return (
+    <div className="tab-item">
+        {icon}
+        <div>
+        <Link className="tab-heading">{heading}</Link>
+        <p className="tab-para">{description}</p>
+        </div>
+    </div>
+  )
+};
+
+export default NavTabItem;
