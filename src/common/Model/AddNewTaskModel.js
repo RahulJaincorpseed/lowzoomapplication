aabfbae9a7e81e44af4a861f818d84236b8abb9f
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-12-19 15:19:11 +0530

    add accordian table

---
+++ src/common/Model/AddNewTaskModel.js
@@ -0,0 +1,226 @@
+
+import React, { useState } from "react"
+import "./Model.css"
+import { priority } from "../../Api/FakeApi"
+import { postQuery } from "../../Api/PostQuery"
+import { useLocation } from "react-router-dom"
+import { customLocation } from "../../Hooks/LocationHook"
+
+const AddNewTaskModel = () => {
+  const [addComplienceData, setAddComplienceData] = useState({
+    name: "",
+    description: "",
+    approvalState: "",
+    applicableZone: "",
+    startDate: "",
+    dueDate: "",
+    completedDate: "",
+    duration: "",
+    // priority: 0,
+    workStatus: 0,
+    enable: true,
+  })
+
+  const location = useLocation();
+
+    const userId = customLocation(1, location)
+    const companyId = customLocation(3, location)
+
+  const complienceDateSetter = (e) => {
+    setAddComplienceData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  console.log("add complience data", addComplienceData)
+
+  const addNewComplienceFun = (e) => {
+    e.preventDefault()
+
+    const complienceAdd = async () => {
+      try {
+        const addNewComplienceRes = await postQuery(
+          `/compliance/company/saveCompliance?companyId=${companyId}`,addComplienceData
+        )
+        console.log("add new complience", addNewComplienceRes)
+        console.log("add new complience", addNewComplienceRes.data)
+        window.location.reload();
+      } catch (err) {
+        console.log("complience err", err)
+      }
+    }
+    complienceAdd()
+  }
+
+  return (
+    <nav>
+      <div className="team-model">
+        <button
+          type="button"
+          className="add-button"
+          data-toggle="modal"
+          data-target="#addNewTaskModel"
+        >
+          {/* <i className="fa-solid fa-pen-to-square mr-1"></i>
+          Edit */}
+          <i className="fa-solid fa-plus"></i> Add New Task Model   
+        </button>
+        {/* MODAL */}
+        <div
+          className="modal fade"
+          id="addNewTaskModel"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalCenterTitle"
+          aria-hidden="true"
+        >
+          <div
+            className="modal-dialog mod-center modal-dialog-centered"
+            role="document"
+          >
+            <div className="modal-content all-center">
+              <div className="add-team-body">
+                {/* START */}
+                <div className="personal-info container">
+                  <h4 className="info-text">Add New Complience</h4>
+                  <form>
+                    <div className="first-form form-row">
+                      <div className="form-group col-md-6">
+                        <div className="pr-ten">
+                          <label className="label-heading" htmlFor="fullName">
+                            Name *
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control input-focus"
+                            id="fullName"
+                            name="name"
+                            onChange={(e) => complienceDateSetter(e)}
+                            placeholder="Enter Name"
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group col-md-6">
+                        <div className="pl-ten">
+                          <label
+                            className="label-heading"
+                            htmlFor="accessTypes"
+                          >
+                            Priority*
+                          </label>
+                          <select
+                            className="form-control input-focus"
+                            name="priority"
+                            id="priority"
+                            onChange={(e) => complienceDateSetter(e)}
+                          >
+                            {priority.map((pri, index) => (
+                              <option key={index} value={pri.id}>
+                                {pri.name}
+                              </option>
+                            ))}
+                          </select>
+                        </div>
+                      </div>
+                      <div className="form-group col-md-6">
+                        <div className="pr-ten">
+                          <label className="label-heading" htmlFor="state">
+                            Select State*
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control input-focus"
+                            id="state"
+                            placeholder="Enter State"
+                            name="approvalState"
+                            onChange={(e) => complienceDateSetter(e)}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group col-md-6">
+                        <div className="pl-ten">
+                          <label className="label-heading" htmlFor="City">
+                            Select City*
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control input-focus"
+                            id="City"
+                            placeholder="Enter City"
+                            name="applicableZone"
+                            onChange={(e) => complienceDateSetter(e)}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group col-md-6">
+                        <div className="pr-ten">
+                          <label className="label-heading" htmlFor="startDate">
+                            Select Start date*
+                          </label>
+                          <input
+                            type="date"
+                            className="form-control input-focus"
+                            id="startDate"
+                            placeholder="Types of Resources"
+                            name="startDate"
+                            onChange={(e) => complienceDateSetter(e)}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group col-md-6">
+                        <div className="pl-ten">
+                          <label className="label-heading" htmlFor="endDate">
+                            Select End date*
+                          </label>
+                          <input
+                            type="date"
+                            className="form-control input-focus"
+                            id="endDate"
+                            placeholder="Add People"
+                            name="completedDate"
+                            onChange={(e) => complienceDateSetter(e)}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group col-md-12">
+                        <div className="pl-ten">
+                          <label className="label-heading" htmlFor="endDate">
+                            Description*
+                          </label>
+                          <textarea
+                            placeholder="Enter Description..."
+                            className="form-control h-twenty input-focus"
+                            name="description"
+                            onChange={(e) => complienceDateSetter(e)}
+                          ></textarea>
+                        </div>
+                      </div>
+                      <div className="all-between-items">
+                        <div className="all-center">
+                          <i className="fa-solid fa-gear"></i>
+                          <h2>Advanced Setting</h2>
+                        </div>
+                        <div>
+                          <button
+                            onClick={(e) => addNewComplienceFun(e)}
+                            className="first-button form-prev-btn"
+                          >
+                            Submit
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default AddNewTaskModel
+
--- src/components/AccordianTable.js
+++ src/components/AccordianTable.js
@@ -1,89 +1,213 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import "./AccordianTable.scss"
+import AddNewTaskModel from "../common/Model/AddNewTaskModel"
 
 const AccordianTable = () => {
-    const [collpaseState, setCollapseState] = useState(false);
+  const [collpaseState, setCollapseState] = useState(false)
   return (
     <div>
-        <div id="accordion" className="tab-data table-responsive">
-  <div className="card">
-    <div className="data-center" id="headingOne">
-        <p>ROC</p>
-        <p>Company Annual Compliences</p>
-        <p>Apply Now</p>
-        <p>already Done</p>
-        <p>Note Applicable</p>
-        <p>LifeTime</p>
-        <p>LifeTime</p>
-        <p>LifeTime</p>
-      <h5 className="mb-0">
-        <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        <i className="fa-solid fa-angle-down"></i>
-        </button>
-      </h5>
-    </div>
+      <div id="accordion" className="tab-data table-responsive">
+        <div className="card">
+          <div className="data-center" id="headingOne">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                <i className="fa-solid fa-angle-down"></i>
+              </button>
+            </h5>
+            <p className="font-boldd mt-0">ROC</p>
+            <p className="font-boldd mt-0 min-des-box">Company Annual Compliences</p>
+            <p className="font-boldd mt-0 min-des-box-new">Apply Now</p>
+            <p  className="font-boldd mt-0 min-des-box-new">already Done</p>
+            <p  className="font-boldd mt-0 min-des-box-new">Note Applicable</p>
+          </div>
 
-    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-      <div className="card-box">
-        <div className="milestone">
-        <p className="mt-0">ROC</p>
-        <p className="min-des">Company Annual Compliences</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">ROC</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">ROC</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">LifeTime</p>
-        <p className="mt-0">LifeTime</p>
+          <div
+            id="collapseOne"
+            className="collapse show"
+            aria-labelledby="headingOne"
+            data-parent="#accordion"
+          >
+            <div className="card-box">
+              <div className="data-box">
+                <div className="milestone">
+                <p className="font-boldd">id</p>
+                  <p className="font-boldd min-des">Task</p>
+                  <p className="min-des font-boldd">Description</p>
+                  <p className="font-boldd min-des-two">Compliance Frequency</p>
+                  <p className="font-boldd min-des-two">Renewal Date (if applicable)</p>
+                  <p className="font-boldd min-des-two">Criticality</p>
+                  <p className="font-boldd min-des-two">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                </div>
+                <div className="milestone">
+                <p className="mt-0">1</p>
+                  <p className="mt-0 min-des">
+                    Register and other records to be maintained by the
+                    contractor
+                  </p>
+                  <p className="min-des mt-0">
+                    It shall be ensured that the contractors are having a valid
+                    license and maintain the statutory register and other
+                    records as prescribed under the Act.{" "}
+                  </p>
+                  <p className="min-des-two">LifeTime</p>
+                  <p className="min-des-two">LifeTime</p>
+                  <p className="min-des-two">Medium</p>
+                  <p className="mt-0">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                </div>
+                <div className="milestone">
+                <p className="mt-0">2</p>
+                  <p className="mt-0 min-des">
+                    Register and other records to be maintained by the
+                    contractor
+                  </p>
+                  <p className="min-des mt-0">
+                    It shall be ensured that the contractors are having a valid
+                    license and maintain the statutory register and other
+                    records as prescribed under the Act.{" "}
+                  </p>
+                  <p className="min-des-two">LifeTime</p>
+                  <p className="min-des-two">LifeTime</p>
+                  <p className="min-des-two">Low</p>
+                  <p className="mt-0">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                </div>
+
+                <div className="milestone">
+                <p className="mt-0">3</p>
+                  <p className="mt-0 min-des">
+                    Register and other records to be maintained by the
+                    contractor
+                  </p>
+                  <p className="min-des mt-0">
+                  It shall be ensured that the contractors pay wages within the time limit prescribed. In case the contractor fails to make payment of wages within the time period of make short payment then the principal employer shall be liable to make the payments of wages in full of the unpaid balance due 
+                  </p>
+                  <p className="min-des-two">LifeTime</p>
+                  <p className="min-des-two">LifeTime</p>
+                  <p className="min-des-two">High</p>
+                  <p className="mt-0">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">ROC</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                  <p className="mt-0">LifeTime</p>
+                </div>
+              
+
+              </div>
+
+            
+
+              <div className="add-milestone">
+                <AddNewTaskModel />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="add-milestone">
-        <input type="text" placeholder="Add New task" />
-        <button>Add</button>
+        <div className="card">
+          <div className="data-center" id="headingTwo">
+            <p>ROC</p>
+            <p>Company Annual Compliences</p>
+            <p>LifeTime</p>
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link collapsed"
+                data-toggle="collapse"
+                data-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                <i className="fa-solid fa-angle-down"></i>
+              </button>
+            </h5>
+          </div>
+          <div
+            id="collapseTwo"
+            className="collapse"
+            aria-labelledby="headingTwo"
+            data-parent="#accordion"
+          >
+            <div className="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life
+              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
+              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
+              on it squid single-origin coffee nulla assumenda shoreditch et.
+              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+              lomo. Leggings occaecat craft beer farm-to-table, raw denim
+              aesthetic synth nesciunt you probably haven't heard of them
+              accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="data-center" id="headingThree">
+            <p>ROC</p>
+            <p>Company Annual Compliences</p>
+            <p>LifeTime</p>
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link collapsed"
+                data-toggle="collapse"
+                data-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                <i className="fa-solid fa-angle-down"></i>
+              </button>
+            </h5>
+          </div>
+          <div
+            id="collapseThree"
+            className="collapse"
+            aria-labelledby="headingThree"
+            data-parent="#accordion"
+          >
+            <div className="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life
+              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
+              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
+              on it squid single-origin coffee nulla assumenda shoreditch et.
+              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+              lomo. Leggings occaecat craft beer farm-to-table, raw denim
+              aesthetic synth nesciunt you probably haven't heard of them
+              accusamus labore sustainable VHS.
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div className="card">
-    <div className="data-center" id="headingTwo">
-    <p>ROC</p>
-        <p>Company Annual Compliences</p>
-        <p>LifeTime</p>
-      <h5 className="mb-0">
-        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        <i className="fa-solid fa-angle-down"></i>
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-      <div className="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div className="card">
-    <div className="data-center" id="headingThree">
-    <p>ROC</p>
-        <p>Company Annual Compliences</p>
-        <p>LifeTime</p>
-      <h5 className="mb-0">
-        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        <i className="fa-solid fa-angle-down"></i>
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-      <div className="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
     </div>
   )
-};
+}
 
-export default AccordianTable;
+export default AccordianTable
